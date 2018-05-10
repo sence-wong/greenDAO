048ec233c1e8d89d346f7e73962b4d23892abff3
SENCE\sence.wong <sence@foxmail.com>
2018-05-10 10:07:50 +0800

    1.编译

--- examples/DaoExample/src/main/java/org/greenrobot/greendao/example/App.java
+++ examples/DaoExample/src/main/java/org/greenrobot/greendao/example/App.java
@@ -2,18 +2,22 @@ package org.greenrobot.greendao.example;
 
 import android.app.Application;
 
+import com.facebook.stetho.Stetho;
+
 import org.greenrobot.greendao.database.Database;
 import org.greenrobot.greendao.example.DaoMaster.DevOpenHelper;
 
 public class App extends Application {
     /** A flag to show how easily you can switch from standard SQLite to the encrypted SQLCipher. */
-    public static final boolean ENCRYPTED = true;
+    public static final boolean ENCRYPTED = false;
 
     private DaoSession daoSession;
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        Stetho.initializeWithDefaults(this);
 
         DevOpenHelper helper = new DevOpenHelper(this, ENCRYPTED ? "notes-db-encrypted" : "notes-db");
         Database db = ENCRYPTED ? helper.getEncryptedWritableDb("super-secret") : helper.getWritableDb();
